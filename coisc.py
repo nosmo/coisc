e7a09277f815d08f7c65bab0563c1f8745d4eb46
Hugh Nowlan <nosmo@nosmo.me>
2020-03-02 21:06:11 +0000

    Add top-level domain blocking

--- coisc.py
+++ coisc.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+"""
+Generate a list of DNS blocks for a given set of hosts.
+
+"""
+
 import argparse
 import os.path
 import sys
 
 import requests
 
+# TODO don't assume these defaults, allow for specification
 # urls that have hostsfile-style notation
 HOST_FILE_URLS = "blocks-hosts_style.txt"
 
 # urls that are just a list of domains
 NONHOST_FILE_URLS = "blocks.txt"
+
+# file containing a list of domains to block all of
+DOMAIN_BLOCK_FILE = "blockdomains.txt"
 
 # IPs in a hosts file to consider safe
 SAFE_IPS = ["0.0.0.0", "127.0.0.1", "255.255.255.255", "::1",
@@ -22,6 +31,10 @@ FILTER_DOMAIN = ["localhost", "localhost.localdomain"]
 REDIRECT_IP = "127.0.0.1"
 
 USER_AGENT = "Coisc (https://github.com/nosmo/coisc)"
+
+
+class UnsupportedFormat(Exception):
+    pass
 
 
 def filter_url_list(url_name, url_list, ip_provided):
@@ -49,7 +62,8 @@ def filter_url_list(url_name, url_list, ip_provided):
             ip, hostname = [ i.strip() for i in url.split() ]
 
             if ip not in SAFE_IPS:
-                print("WARNING!! Unsafe IP found in file %s: %s" % (url_name, url))
+                print("WARNING!! Unsafe IP found in file {}: {}".format(
+                    url_name, url))
                 raise SystemExit
         else:
             hostname = url
@@ -66,17 +80,19 @@ def process_url_dict(url_dict, ip_provided):
     """
     for label, url in url_dict.items():
         print("Processing %s" % label)
-        downloaded_req = requests.get(url, headers = {"User-Agent": USER_AGENT})
+        downloaded_req = requests.get(url, headers={"User-Agent": USER_AGENT})
         if downloaded_req.ok:
             downloaded_list = downloaded_req.text.strip().split("\n")
-            filtered_list = filter_url_list(label, downloaded_list, ip_provided)
+            filtered_list = filter_url_list(label, downloaded_list,
+                                            ip_provided)
         else:
-            sys.stderr.write("Failed to download URL %s: %s" % (url, downloaded_req.text))
+            sys.stderr.write("Failed to download URL {}: {}\n".format(url, downloaded_req.text))
     return filtered_list
 
 
+# TODO make these their own class
 def format_dnsmasq(redirect_ip, domain):
-    return "address=/%s/%s\n" % (domain, redirect_ip)
+    return "address=/{}/{}\n".format(domain, redirect_ip)
 
 
 def extract_dnsmasq(record_string):
@@ -84,7 +100,7 @@ def extract_dnsmasq(record_string):
 
 
 def format_hosts(redirect_ip, domain):
-    return "%s\t%s\n" % (redirect_ip, domain)
+    return "{}\t{}\n".format(redirect_ip, domain)
 
 
 def extract_hosts(record_string):
@@ -92,11 +108,35 @@ def extract_hosts(record_string):
 
 
 def format_bind(redirect_ip, domain):
-    return "%s.\tIN\tA\t%s\n" % (domain, redirect_ip)
+    return "{}.\tIN\tA\t{}\n".format(domain, redirect_ip)
 
 
 def extract_bind(record_string):
     return record_string.split()[0]
+
+
+def domain_block_dnsmasq(redirect_ip, domain):
+    """Domain-wide block for a domain using dnsmasq
+    """
+
+    return "domain=/{}/{}\n".format(domain, redirect_ip)
+
+
+def domain_block_hosts(redirect_ip, domain):
+    """Domain-wide block for a domain using hostsfile
+
+    Not supported for hostsfile due to lack of support
+    """
+
+    raise UnsupportedFormat("No top-level block support for hosts file")
+
+
+def domain_block_bind(redirect_ip, domain):
+    """Domain-wide block for a domain using bind
+
+    Not supported for bind yet.
+    """
+    raise UnsupportedFormat("No top-level block support for bind")
 
 
 EXTRACT_DICT = {
@@ -111,6 +151,13 @@ OUTPUT_DICT = {
     "bind": format_bind
 }
 
+DOMAIN_OUTPUT_DICT = {
+    "dnsmasq": domain_block_dnsmasq,
+    "hosts": domain_block_hosts,
+    "bind": domain_block_bind
+}
+
+
 def urlfile_to_dict(file_path):
     url_dict = {}
     with open(file_path) as url_f:
@@ -121,8 +168,16 @@ def urlfile_to_dict(file_path):
                 url_dict[url_label] = url
     return url_dict
 
-def main(output_format, output_path, add_mode):
+
+def main(output_format, output_path, add_mode, domain_block_files):
+
     full_domain_list = []
+    domain_blocks = []
+    for domain_file in domain_block_files:
+        with open(domain_file) as domain_f:
+            for l in domain_f.read().strip().split("\n"):
+                domain_blocks.append(l)
+
     full_domain_list += process_url_dict(urlfile_to_dict(HOST_FILE_URLS), True)
     full_domain_list += process_url_dict(urlfile_to_dict(NONHOST_FILE_URLS), False)
     print("Got %d entries" % len(full_domain_list))
@@ -131,16 +186,30 @@ def main(output_format, output_path, add_mode):
     existing_list = []
     if os.path.exists(output_path):
         with open(output_path, "r") as output_f:
+            # TODO this function isn't called - why?
             extract_function = EXTRACT_DICT[output_format]
-            existing_list = [ i.strip().split()[1] for i in output_f.read().split("\n") \
-                              if i.strip() ]
-
+            existing_list = [extract_function(i.strip())
+                             for i in output_f.read().split("\n")
+                             if i.strip()]
     if add_mode:
         full_domain_list = set(full_domain_list + existing_list)
+
+    if domain_blocks:
+        print("Got {} domain blocks".format(len(domain_blocks)))
 
     with open(output_path, "w") as output_f:
         for domain in full_domain_list:
             output_f.write(OUTPUT_DICT[output_format](REDIRECT_IP, domain))
+
+        for domain_block in domain_blocks:
+            try:
+                output_f.write(DOMAIN_OUTPUT_DICT[output_format](REDIRECT_IP, domain_block))
+            except UnsupportedFormat as exc:
+                sys.stderr.write(
+                    "Not writing domain block for unsupported format {}: {}\n".format(
+                        output_format, exc))
+                break
+
     print("Complete - wrote using %s format to %s" % (output_format, output_path))
     if not add_mode and existing_list and existing_list != full_domain_list:
         print("Added %d entries" % len(set(full_domain_list).difference(existing_list)))
@@ -149,6 +218,8 @@ def main(output_format, output_path, add_mode):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Generate system-level advertising/malware blocklists')
+    parser.add_argument("--domainlist", "-D", dest="domain_blocks", action="store", default="",
+                        help="File to read domain blocks from", required=True, nargs='*')
     parser.add_argument("--output", "-o", dest="output_path", action="store", default="",
                         help="Where to write output", required=True)
     parser.add_argument("--format", "-f", dest="output_format", action="store", default="hosts",
@@ -157,4 +228,4 @@ if __name__ == "__main__":
                         help="Don't remove any lines, only add new ones")
     args = parser.parse_args()
 
-    main(args.output_format, args.output_path, args.add)
+    main(args.output_format, args.output_path, args.add, args.domain_blocks)
